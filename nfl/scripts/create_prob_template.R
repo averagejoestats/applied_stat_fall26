89e3d044b8e14dc2287d7f302d98de866352fa82
Joe Guinness <joeguinness@gmail.com>
2026-09-04 10:41:52 -0500

    change names back

--- nfl/scripts/create_prob_template.R
+++ nfl/scripts/create_prob_template.R
@@ -8,11 +8,9 @@ dat[,7:10]
 
 colnames(dat)[3] <- "date"
 
-vars <- c("Week","date","HomeTm","VisTm","Time","home_prob","away_prob")
+vars <- c("Week","date","VisTm","HomeTm","Time","home_prob","away_prob")
 dat <- dat[ vars ]
 
-colnames(dat)[3] <- "home_team"
-colnames(dat)[4] <- "away_team"
 head( dat )
 
 fname <- "../templates/game_prob_template_2026.csv"
