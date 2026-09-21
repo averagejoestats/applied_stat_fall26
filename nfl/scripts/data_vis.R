662a707607b03af87821da2bdac6c59623839e30
Joe Guinness <joeguinness@gmail.com>
2026-09-21 12:54:29 -0500

    formatting changes made during lecture

--- nfl/scripts/data_vis.R
+++ nfl/scripts/data_vis.R
@@ -25,8 +25,10 @@ for(j in 1:nrow(team_stats)){
     i2024 <- dat$year == 2024
     i2025 <- dat$year == 2025
     x <- dat$outcome
-    team_stats$wins_2024[j] <- sum( x[ ihome & i2024 ]) + sum(1 - x[ iaway & i2024 ] )
-    team_stats$wins_2025[j] <- sum( x[ ihome & i2025 ]) + sum(1 - x[ iaway & i2025 ] )
+    team_stats$wins_2024[j] <-
+        sum( x[ ihome & i2024 ]) + sum(1 - x[ iaway & i2024 ] )
+    team_stats$wins_2025[j] <-
+        sum( x[ ihome & i2025 ]) + sum(1 - x[ iaway & i2025 ] )
     team_stats$playoffs_2024[j] <- any(
         dat$playoffs & i2024 & (dat$home_team == tt | dat$away_team == tt )
     )
@@ -36,19 +38,23 @@ for(j in 1:nrow(team_stats)){
 }
 
 # create an abbreviation
-team_stats$abbr <- c("KC","PHL","ATL","BUF","NO","CHI","CIN","IND","MIA","NYG","SEA","LAC",
-                     "CLE","TAM","DET","SF","CAR","JAX","GB","DAL","MN","NE","WAS","TN",
-                     "BAL","AZ","DEN","HOU","NYJ","PIT","LV","LAR")
+team_stats$abbr <- c(
+    "KC","PHL","ATL","BUF","NO","CHI","CIN","IND","MIA","NYG","SEA","LAC",
+    "CLE","TAM","DET","SF","CAR","JAX","GB","DAL","MN","NE","WAS","TN",
+    "BAL","AZ","DEN","HOU","NYJ","PIT","LV","LAR"
+)
 
     
 # first try
-plot(team_stats$wins_2024, team_stats$wins_2025, xlim = c(0,17), ylim = c(0,17),
+plot(team_stats$wins_2024, team_stats$wins_2025,
+     xlim = c(0,17), ylim = c(0,17),
      xlab = "2024 wins", ylab = "2025 wins"
 )
 
 
 # Add teams
-plot(team_stats$wins_2024, team_stats$wins_2025, type = "n", xlim = c(0,17), ylim = c(0,17),
+plot(team_stats$wins_2024, team_stats$wins_2025, type = "n",
+     xlim = c(0,17), ylim = c(0,17),
      xlab = "2024 wins", ylab = "2025 wins"
 )
 text( team_stats$wins_2024, team_stats$wins_2025, team_stats$abbr )
@@ -61,10 +67,14 @@ team_stats$offset[28] <- 0.15
 team_stats$offset[32] <- -0.15
 team_stats$offset[11] <- 0.15
 team_stats$offset[27] <- -0.15
-plot(team_stats$wins_2024, team_stats$wins_2025, type = "n", xlim = c(0,17), ylim = c(0,17),
+plot(team_stats$wins_2024, team_stats$wins_2025, type = "n",
+     xlim = c(0,17), ylim = c(0,17),
      xlab = "2024 wins", ylab = "2025 wins"
 )
-text( team_stats$wins_2024, team_stats$wins_2025 + team_stats$offset, team_stats$abbr )
+abline(0,1, col = "lightgray", lwd = 1)
+text( team_stats$wins_2024, team_stats$wins_2025 + team_stats$offset,
+     team_stats$abbr
+)
 
     
 # set the axes yourself and add gridlines
@@ -75,13 +85,15 @@ team_stats$offset[28] <- 0.15
 team_stats$offset[32] <- -0.15
 team_stats$offset[11] <- 0.15
 team_stats$offset[27] <- -0.15
-plot(team_stats$wins_2024, team_stats$wins_2025, type = "n", xlim = c(0,17), ylim = c(0,17),
+plot(team_stats$wins_2024, team_stats$wins_2025, type = "n",
+     xlim = c(0,17), ylim = c(0,17),
      xlab = "2024 wins", ylab = "2025 wins", axes = FALSE
 )
 for(j in 0:17){
     abline(v=j, col = "lightgray")
     abline(h=j, col = "lightgray")
 }
+abline(0,1, col = "lightgray", lwd = 1)
 text( team_stats$wins_2024, team_stats$wins_2025 + team_stats$offset, team_stats$abbr )
 axis(1, at = 1:17)
 axis(2, at = 1:17)
@@ -103,11 +115,13 @@ for(j in 0:17){
     abline(v=j, col = "lightgray")
     abline(h=j, col = "lightgray")
 }
+abline(0,1, col = "lightgray", lwd = 1)
 colors <- rep("black",nrow(team_stats))
 colors[ team_stats$playoffs_2024 & !team_stats$playoffs_2025 ] <- "blue"
 colors[ !team_stats$playoffs_2024 & team_stats$playoffs_2025 ] <- "red"
 colors[ team_stats$playoffs_2024 & team_stats$playoffs_2025 ] <- "purple"
-text( team_stats$wins_2024, team_stats$wins_2025 + team_stats$offset, team_stats$abbr, col = colors, font = 2)
+text( team_stats$wins_2024, team_stats$wins_2025 + team_stats$offset,
+     team_stats$abbr, col = colors, font = 2)
 axis(1, at = 0:17)
 axis(2, at = 0:17)
 box()
